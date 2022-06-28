dc075bdac1c520d868bda46b59729e0fbc6e84b2
Toni Itkonen <toni.itkonen@icloud.com>
2022-06-28 14:26:08 +0300

    MapType picker added.

--- Mapkit-userLocations/ContentView.swift
+++ Mapkit-userLocations/ContentView.swift
@@ -3,16 +3,34 @@ import SwiftUI
 
 struct ContentView: View {
   @StateObject private var viewModel = ContentViewModel()
+//  @State private var mapType: MKMapType = .satellite
   
     var body: some View {
-      Map(coordinateRegion: $viewModel.region, showsUserLocation: true)
-        .ignoresSafeArea()
-        .accentColor(Color(.systemPink))
-        .onAppear {
-          viewModel.checkIfLocationManagerIsEnabled()
-        }
-    }
-}
+      ZStack {
+        Map(coordinateRegion: $viewModel.region,
+            showsUserLocation: true
+//            mapType: $viewModel.mapType
+        )
+          .ignoresSafeArea()
+          .accentColor(Color(.systemPink))
+          
+          .onAppear {
+            viewModel.checkIfLocationManagerIsEnabled()
+          }
+        
+        VStack {
+          Picker("", selection: $viewModel.mapType) {
+            Text("Standard").tag(MKMapType.standard)
+            Text("Satellite").tag(MKMapType.satellite)
+            Text("Hybrid").tag(MKMapType.hybrid)
+          }
+          .pickerStyle(SegmentedPickerStyle())
+          .offset(y:340)
+          
+        } ///_VStack
+      } /// _ZStack
+    } ///_Body
+} /// _Struct
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
@@ -26,8 +44,20 @@ struct ContentView_Previews: PreviewProvider {
 /// CLLocationManagerDelegate handles the changed status of locationManager
 final class ContentViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
   
+  @Published var mapType = MKMapType.satellite
+  
   @Published var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 60.1699, longitude: 24.9384), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
   var locationManager: CLLocationManager?
+  
+//  func getMapType() -> MKMapType {
+//         switch mapType {
+//         case 0: return .standard
+//         case 1: return .satellite
+//         case 2: return .hybrid
+//         default:
+//             return .standard
+//         }
+//  }
   
   func checkIfLocationManagerIsEnabled() {
     if CLLocationManager.locationServicesEnabled() {
